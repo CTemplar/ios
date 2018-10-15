84d427b64be97e8f75a583d3e325a86a5984b878
Tatarinov Dmitry <support@hypertunnels3d.com>
2018-10-15 14:01:18 +0300

    IAD-46 - change presentation method to show(); - implemented Title and Back Buttons for ForgotPassword and ConfirmResetPassword Controllers;

--- Ctemplar/Presentation/ForgotPasswordsScreens/ConfirmResetPasswordViewController.swift
+++ Ctemplar/Presentation/ForgotPasswordsScreens/ConfirmResetPasswordViewController.swift
@@ -15,32 +15,13 @@ class ConfirmResetPasswordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        setupNavigationBar()
+
     }
     
-    func setupNavigationBar() {
-        /*
-        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        // Sets shadow (line below the bar) to a blank image
-        UINavigationBar.appearance().shadowImage = UIImage()
-        // Sets the translucent background color
-        UINavigationBar.appearance().backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
-        // Set translucent. (Default value is already true, so this can be removed if desired.)
-        UINavigationBar.appearance().isTranslucent = true
- 
-        */
-        
-        navigationController?.navigationBar.backgroundColor = UIColor(red: 255.0, green: 0.0, blue: 0.0, alpha: 0.0)
-        
-    }
+    //MARK: - IBActions
     
-    func setDefaultNavigationBar() {
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
         
-        UINavigationBar.appearance().setBackgroundImage(nil, for: UIBarMetrics.default)
-        // Sets shadow (line below the bar) to a blank image
-        UINavigationBar.appearance().shadowImage = nil
-        // Sets the translucent background color
-        //UINavigationBar.appearance().backgroundColor = k_whiteColor
+        self.dismiss(animated: true, completion: nil)
     }
 }
--- Ctemplar/Presentation/ForgotPasswordsScreens/ForgotPasswordViewController.swift
+++ Ctemplar/Presentation/ForgotPasswordsScreens/ForgotPasswordViewController.swift
@@ -16,13 +16,12 @@ class ForgotPasswordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //setupNavigationBar()
-
     }
+    
+    //MARK: - IBActions
     
     @IBAction func backButtonPressed(_ sender: AnyObject) {
         
-        setDefaultNavigationBar()
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -31,33 +30,12 @@ class ForgotPasswordViewController: UIViewController {
         //temp
         let storyboard: UIStoryboard = UIStoryboard(name: k_ForgotPasswordStoryboardName, bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: k_ConfirmResetPasswordViewControllerID) as! ConfirmResetPasswordViewController
-        self.show(vc, sender: self)
-        //self.present(vc, animated: true, completion: nil)
-       
+        self.present(vc, animated: true, completion: nil)
+        
     }
     
     @IBAction func forgotUsernameButtonPressed(_ sender: AnyObject) {
         
         
-    }
-    
-    func setupNavigationBar() {
-        
-        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        // Sets shadow (line below the bar) to a blank image
-        UINavigationBar.appearance().shadowImage = UIImage()
-        // Sets the translucent background color
-        UINavigationBar.appearance().backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
-        // Set translucent. (Default value is already true, so this can be removed if desired.)
-        UINavigationBar.appearance().isTranslucent = true
-    }
-    
-    func setDefaultNavigationBar() {
-        
-        UINavigationBar.appearance().setBackgroundImage(nil, for: UIBarMetrics.default)
-        // Sets shadow (line below the bar) to a blank image
-        UINavigationBar.appearance().shadowImage = nil
-        // Sets the translucent background color
-        //UINavigationBar.appearance().backgroundColor = k_whiteColor
     }
 }
