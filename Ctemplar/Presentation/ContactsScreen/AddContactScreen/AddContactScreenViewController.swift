b98efb58f616b1d21c63d4b9fbb2c8d40e368817
Tatarinov Dmitry <support@hypertunnels3d.com>
2018-11-20 11:59:29 +0300

    IAD-170 setup UI elements for Add Contact Screen;

--- Ctemplar/Presentation/ContactsScreen/AddContactScreen/AddContactScreenViewController.swift
+++ Ctemplar/Presentation/ContactsScreen/AddContactScreen/AddContactScreenViewController.swift
@@ -9,14 +9,27 @@
 import Foundation
 import UIKit
 
-class AddContactViewController: UIViewController {
+class AddContactViewController: UIViewController, UITextFieldDelegate {
     
     var navBarTitle: String? = ""
+    
+    var contactName     : String? = ""
+    var contactEmail    : String? = ""
+    var contactPhone    : String? = ""
+    var contactAddress  : String? = ""
+    var contactNote     : String? = ""
+    
+    @IBOutlet var contactNameTextField      : UITextField!
+    @IBOutlet var contactEmailTextField     : UITextField!
+    @IBOutlet var contactPhoneTextField     : UITextField!
+    @IBOutlet var contactAddressTextField   : UITextField!
+    @IBOutlet var contactNoteTextField      : UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem?.isEnabled = false
+        self.navigationItem.rightBarButtonItem = nil
         self.navigationItem.title = navBarTitle
     }
     
@@ -29,5 +42,15 @@ class AddContactViewController: UIViewController {
     
     @IBAction func saveButtonPressed(_ sender: AnyObject) {
   
+    }
+    
+    @IBAction func textFieldTyped(_ sender: UITextField) {
+        
+        print("typed:", sender.text as Any)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //print("input:", textField.text as Any)
     }
 }
