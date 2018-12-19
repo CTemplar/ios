900a1a2319f3b704c6dd5e9507a81588f56a0e26
Tatarinov Dmitry <support@hypertunnels3d.com>
2018-12-19 15:46:32 +0300

    IAD-248 implement Colors for Buttons for Color Picker; - add rounded rect for Buttons;

--- Ctemplar/Presentation/AddFolder/ColorPickerView.swift
+++ Ctemplar/Presentation/AddFolder/ColorPickerView.swift
@@ -21,6 +21,23 @@ class ColorPickerView: UIView {
     let k_buttonsRows = 2
     let k_spaceBetweenButtons : CGFloat = 10.0
     
+    let k_color1: UIColor = UIColor(red: 114.0 / 255.0, green: 114.0 / 255.0, blue: 168.0 / 255.0, alpha: 1.0)
+    let k_color2: UIColor = UIColor(red: 208.0 / 255.0, green: 88.0 / 255.0, blue: 89.0 / 255.0, alpha: 1.0)
+    let k_color3: UIColor = UIColor(red: 194.0 / 255.0, green: 108.0 / 255.0, blue: 199.0 / 255.0, alpha: 1.0)
+    let k_color4: UIColor = UIColor(red: 117.0 / 255.0, green: 104.0 / 255.0, blue: 209.0 / 255.0, alpha: 1.0)
+    let k_color5: UIColor = UIColor(red: 106.0 / 255.0, green: 169.0 / 255.0, blue: 210.0 / 255.0, alpha: 1.0)
+    let k_color6: UIColor = UIColor(red: 95.0 / 255.0, green: 199.0 / 255.0, blue: 184.0 / 255.0, alpha: 1.0)
+    let k_color7: UIColor = UIColor(red: 114.0 / 255.0, green: 187.0 / 255.0, blue: 116.0 / 255.0, alpha: 1.0)
+    let k_color8: UIColor = UIColor(red: 196.0 / 255.0, green: 210.0 / 255.0, blue: 97.0 / 255.0, alpha: 1.0)
+    let k_color9: UIColor = UIColor(red: 230.0 / 255.0, green: 193.0 / 255.0, blue: 76.0 / 255.0, alpha: 1.0)
+    let k_color10: UIColor = UIColor(red: 230.0 / 255.0, green: 153.0 / 255.0, blue: 77.0 / 255.0, alpha: 1.0)
+    let k_color11: UIColor = UIColor(red: 207.0 / 255.0, green: 126.0 / 255.0, blue: 125.0 / 255.0, alpha: 1.0)
+    let k_color12: UIColor = UIColor(red: 200.0 / 255.0, green: 147.0 / 255.0, blue: 203.0 / 255.0, alpha: 1.0)
+    let k_color13: UIColor = UIColor(red: 156.0 / 255.0, green: 148.0 / 255.0, blue: 208.0 / 255.0, alpha: 1.0)
+    let k_color14: UIColor = UIColor(red: 169.0 / 255.0, green: 196.0 / 255.0, blue: 213.0 / 255.0, alpha: 1.0)
+    
+    var colorsArray = [UIColor]()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -33,7 +50,7 @@ class ColorPickerView: UIView {
     
     private func commonInit() {
         
-        //self.backgroundColor = UIColor.green
+        colorsArray = [k_color1, k_color2, k_color3, k_color4, k_color5, k_color6, k_color7, k_color8, k_color9, k_color10, k_color11, k_color12, k_color13, k_color14]
     }
     
     func setupColorPicker(width: CGFloat, height: CGFloat) {        
@@ -51,13 +68,12 @@ class ColorPickerView: UIView {
             for indexX in 0...k_buttonsInRow - 1 {
             
                 let xPosition = (buttonWidth + k_spaceBetweenButtons) * CGFloat(indexX)
+   
+                index = index + 1        
             
-                index = indexX + indexY
-            
-                self.createButton(x: xPosition, y: yPosition, buttonWidth: buttonWidth, tag: index + 1)
+                self.createButton(x: xPosition, y: yPosition, buttonWidth: buttonWidth, tag: index)
             }
         }
-            
     }
     
     func calculateButtonWidth(viewWidth: CGFloat) -> CGFloat {
@@ -72,7 +88,10 @@ class ColorPickerView: UIView {
         
         let frame = CGRect(x: x, y: y, width: buttonWidth, height: buttonWidth)
         let button = UIButton(frame: frame)
-        button.backgroundColor = UIColor.orange
+        button.backgroundColor = colorsArray[tag - 1]
+        button.layer.cornerRadius = buttonWidth / 2
+        button.layer.borderWidth = 0
+        button.layer.borderColor = UIColor.black.cgColor
         button.tag = tag
         self.add(subview: button)
     }
