cd4d5a71c42efbf711b109fe6c270a5c65a5a46c
Tatarinov Dmitry <support@hypertunnels3d.com>
2018-12-20 09:20:11 +0300

    IAD-252 implemented Edit Folder View Controller;

---
+++ Ctemplar/Presentation/EditFolder/EditFolderInteractor.swift
@@ -0,0 +1,66 @@
+//
+//  EditFolderInteractor.swift
+//  Ctemplar
+//
+//  Created by Tatarinov Dmitry on 20.12.2018.
+//  Copyright © 2018 ComeOnSoftware. All rights reserved.
+//
+
+import Foundation
+import AlertHelperKit
+import PKHUD
+
+class EditFolderInteractor {
+    
+    var viewController  : EditFolderViewController?
+    var apiService      : APIService?
+    var formatterService        : FormatterService?
+    
+    func validateFolderName(text: String) {
+        
+        var nameValid : Bool = false
+        
+        if (self.formatterService?.validateNameFormat(enteredName: text))! {
+            self.viewController?.folderName = text
+            self.viewController?.darkLineView.backgroundColor = k_sideMenuColor
+            nameValid = true
+        } else {
+            self.viewController?.darkLineView.backgroundColor = k_redColor
+            self.setAddButton(enable: true)
+            nameValid = false
+        }
+        
+        if (self.viewController?.selectedHexColor.count)! > 0 && nameValid {
+            self.setAddButton(enable: true)
+        } else {
+            self.setAddButton(enable: false)
+        }
+    }
+    
+    func setAddButton(enable: Bool) {
+        
+        if enable {
+            //self.viewController?.addButton.isEnabled = true
+            //self.viewController?.addButton.alpha = 1.0
+        } else {
+            //self.viewController?.addButton.isEnabled = false
+           //self.viewController?.addButton.alpha = 0.6
+        }
+    }
+    
+    func createCustomFolder(name: String, colorHex: String) {
+        
+        apiService?.createCustomFolder(name: name, color: colorHex) {(result) in
+            
+            switch(result) {
+                
+            case .success(let value):
+                print("value:", value)
+                self.viewController?.dismiss(animated: true, completion: nil)
+            case .failure(let error):
+                print("error:", error)
+                AlertHelperKit().showAlert(self.viewController!, title: "Create Folder Error", message: error.localizedDescription, button: "closeButton".localized())
+            }
+        }
+    }
+}
---
+++ Ctemplar/Presentation/EditFolder/EditFolderViewController.swift
@@ -0,0 +1,78 @@
+//
+//  EditFolderViewController.swift
+//  Ctemplar
+//
+//  Created by Tatarinov Dmitry on 20.12.2018.
+//  Copyright © 2018 ComeOnSoftware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditFolderViewController: UIViewController {
+    
+    var interactor       : EditFolderInteractor?
+    
+    @IBOutlet var deleteButton              : UIButton!
+    
+    @IBOutlet var folderNameTextField       : UITextField!
+    @IBOutlet var darkLineView              : UIView!
+    @IBOutlet var colorPickerSuperView      : UIView!
+    
+    var selectedHexColor : String = ""
+    var folderName : String = ""
+    
+    var colorPicker : ColorPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let configurator = EditFolderConfigurator()
+        configurator.configure(viewController: self)
+        
+        self.colorPicker = ColorPickerView()
+        self.colorPicker.delegate = self
+        self.colorPickerSuperView.add(subview: colorPicker)
+        
+        self.interactor?.validateFolderName(text: self.folderName)        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.colorPicker.setupColorPicker(width: self.colorPickerSuperView.bounds.width, height: self.colorPickerSuperView.bounds.height)
+        
+        self.folderNameTextField.becomeFirstResponder()
+    }
+    
+    //MARK: - IBActions
+    
+    @IBAction func deleteButtonPressed(_ sender: AnyObject) {
+        
+       
+    }
+        
+    @IBAction func saveButtonPressed(_ sender: AnyObject) {
+        
+        
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: AnyObject) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func textTyped(_ sender: UITextField) {
+        
+        self.interactor?.validateFolderName(text: sender.text!)
+    }
+}
+
+extension EditFolderViewController: ColorPickerViewDelegate {
+    
+    func selectColorAction(colorHex: String) {
+        print("selected colorHex:", colorHex)
+        self.selectedHexColor = colorHex
+        self.interactor?.validateFolderName(text: self.folderName)
+    }
+}
