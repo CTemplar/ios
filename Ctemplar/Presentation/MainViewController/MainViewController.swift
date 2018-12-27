e4d2206aded8bcff3031088ede0e29d2161700f7
Tatarinov Dmitry <support@hypertunnels3d.com>
2018-12-27 10:56:29 +0300

    IAD-284 setup Navigation Bar and Segment Control for White/Black lists Screen;

--- Ctemplar/Presentation/MainViewController/MainViewController.swift
+++ Ctemplar/Presentation/MainViewController/MainViewController.swift
@@ -145,6 +145,26 @@ class DesignableButton: UIButton {
 class DesignableLabel: UILabel {
 }
 
+extension UINavigationBar {
+    
+    func showBorderLine() {
+        findBorderLine().isHidden = false
+    }
+    
+    func hideBorderLine() {
+        findBorderLine().isHidden = true
+    }
+    
+    private func findBorderLine() -> UIImageView! {
+        return self.subviews
+            .flatMap { $0.subviews }
+            .compactMap { $0 as? UIImageView }
+            .filter { $0.bounds.size.width == self.bounds.size.width }
+            .filter { $0.bounds.size.height <= 2 }
+            .first
+    }
+}
+
 extension UIView {
     
     func addBlurEffect() {
--- Ctemplar/Presentation/WhiteBlackLists/WhiteBlackListsViewController.swift
+++ Ctemplar/Presentation/WhiteBlackLists/WhiteBlackListsViewController.swift
@@ -11,15 +11,74 @@ import UIKit
 
 class WhiteBlackListsViewController: UIViewController {
     
+    @IBOutlet var segmentedControl      : UISegmentedControl!
+    
+    @IBOutlet var tableView             : UITableView!
+    
+    @IBOutlet var underlineView         : UIView!
+    
+    @IBOutlet var underlineWidthConstraint              : NSLayoutConstraint!
+    @IBOutlet var underlineLeftOffsetConstraint         : NSLayoutConstraint!
+    
     var user = UserMyself()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setupSegmentedControl()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        navigationController!.navigationBar.hideBorderLine()
     }
     
     @IBAction func backButtonPressed(_ sender: AnyObject) {
         
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        
+        let selectedIndex = self.segmentedControl.selectedSegmentIndex
+        
+        var selected = false
+        
+        if selectedIndex == 0 {
+            selected = true
+        } else {
+            selected = false
+        }
+        
+        self.setupUnderlineView(leftButtonSelected: selected)
+    }
+    
+    func setupSegmentedControl() {
+        
+        self.segmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.font : UIFont(name: k_latoRegularFontName, size: 14)!,
+            NSAttributedString.Key.foregroundColor: k_sideMenuTextFadeColor
+            ], for: .normal)
+        
+        self.segmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.font : UIFont(name: k_latoBoldFontName, size: 14)!,
+            NSAttributedString.Key.foregroundColor: k_redColor
+            ], for: .selected)
+        
+        
+        self.setupUnderlineView(leftButtonSelected: true)
+    }
+    
+    func setupUnderlineView(leftButtonSelected: Bool) {
+       
+        let width = self.view.frame.width / 2.0
+        self.underlineWidthConstraint.constant = width
+        
+        if leftButtonSelected {
+            self.underlineLeftOffsetConstraint.constant = 0.0
+        } else {
+            self.underlineLeftOffsetConstraint.constant = width
+        }
     }
 }
