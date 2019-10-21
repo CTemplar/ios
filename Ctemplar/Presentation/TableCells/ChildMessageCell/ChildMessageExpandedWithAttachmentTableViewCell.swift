8e67aeaf4996de924645be8299ba5235005bccc3
Tatarinov Dmitry <support@hypertunnels3d.com>
2019-10-21 14:20:49 +0300

    IAD-440 implemented property Label for Child Message Expanded with Attachment Table View Cell;

--- Ctemplar/Presentation/TableCells/ChildMessageCell/ChildMessageExpandedWithAttachmentTableViewCell.swift
+++ Ctemplar/Presentation/TableCells/ChildMessageCell/ChildMessageExpandedWithAttachmentTableViewCell.swift
@@ -24,6 +24,17 @@ class ChildMessageExpandedWithAttachmentTableViewCell: UITableViewCell, UICollec
     
     @IBOutlet var collectionView       : UICollectionView!
     
+    @IBOutlet weak var deleteLabel             : UILabel!
+    @IBOutlet weak var leftLabel               : UILabel!
+     
+    @IBOutlet weak var timerlabelsView         : UIView!
+    @IBOutlet weak var leftlabelView           : UIView!
+    @IBOutlet weak var rightlabelView          : UIView!
+    
+    @IBOutlet var timerlabelsViewWidthConstraint        : NSLayoutConstraint!
+    @IBOutlet var leftlabelViewWidthConstraint          : NSLayoutConstraint!
+    @IBOutlet var rightlabelViewWidthConstraint         : NSLayoutConstraint!
+    
     @IBOutlet var fromToViewHeightConstraint        : NSLayoutConstraint!
     @IBOutlet var fromToBarTextViewHeightConstraint : NSLayoutConstraint!
     
@@ -81,6 +92,70 @@ class ChildMessageExpandedWithAttachmentTableViewCell: UITableViewCell, UICollec
             self.attachmentsArray = attachments
             self.collectionView.reloadData()
         }
+    
+        self.setupPropertyLabel(message: message)
+    }
+        
+    func setupPropertyLabel(message: EmailMessage) {
+          
+        leftlabelView.isHidden = true
+          
+        if let delayedDelivery = message.delayedDelivery {
+            leftlabelView.isHidden = false
+            leftlabelView.backgroundColor = k_greenColor
+            if  let date = parentController?.formatterService!.formatDestructionTimeStringToDate(date: delayedDelivery) {
+                leftLabel.attributedText = date.timeCountForDelivery(short: false)
+            } else {
+                if let date = parentController?.formatterService!.formatDestructionTimeStringToDateTest(date: delayedDelivery) {
+                    leftLabel.attributedText = date.timeCountForDelivery(short: false)
+                } else {
+                    leftLabel.attributedText = NSAttributedString(string: "Error")
+                }
+            }
+        }
+          
+        if let deadManDuration = message.deadManDuration {
+            leftlabelView.isHidden = false
+            leftlabelView.backgroundColor = k_redColor
+            if  let date = parentController?.formatterService!.formatDeadManDateString(duration: deadManDuration, short: false) {
+                leftLabel.attributedText = date
+            } else {
+                leftLabel.attributedText = NSAttributedString(string: "Error")
+            }
+        }
+    
+        if let destructionDate = message.destructDay {
+            rightlabelView.isHidden = false
+            rightlabelView.backgroundColor = k_orangeColor
+            if  let date = parentController?.formatterService!.formatDestructionTimeStringToDate(date: destructionDate) {
+                deleteLabel.attributedText = date.timeCountForDestruct(short: false)
+            } else {
+                //print("erorr formatting destructionDate:", destructionDate)
+                if let date = parentController?.formatterService!.formatDestructionTimeStringToDateTest(date: destructionDate) {
+                    print("new format date:", date)
+                    deleteLabel.attributedText = date.timeCountForDestruct(short: false)
+                } else {
+                    deleteLabel.attributedText = NSAttributedString(string: "Error")
+                }
+            }
+        } else {
+            rightlabelView.isHidden = true
+        }
+                  
+        leftlabelViewWidthConstraint.constant = k_deleteLabelWidth
+        rightlabelViewWidthConstraint.constant = k_deleteLabelWidth
+                
+        if leftlabelView.isHidden {
+            leftlabelViewWidthConstraint.constant = 0.0
+        }
+
+        if rightlabelView.isHidden {
+            rightlabelViewWidthConstraint.constant = 0.0
+        }
+                
+        timerlabelsViewWidthConstraint.constant = leftlabelViewWidthConstraint.constant + rightlabelViewWidthConstraint.constant
+          
+        self.layoutIfNeeded()
     }
     
     func setupFromToHeaderHeight(message: EmailMessage) {
