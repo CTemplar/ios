14ba5b46ed1089770c704bf1918a1e8c5abca6ed
Developer <gupta.nivrit@gmail.com>
2020-06-04 13:04:58 +0530

    IAD-537
    Links should not open within the message window. They should be opened in Safari or in an in-app browser using WKWebView

--- Ctemplar/Presentation/TableCells/ChildMessageCell/ChildMessageExpandedWithAttachmentTableViewCell.swift
+++ Ctemplar/Presentation/TableCells/ChildMessageCell/ChildMessageExpandedWithAttachmentTableViewCell.swift
@@ -304,6 +304,25 @@ extension ChildMessageExpandedWithAttachmentTableViewCell: WKNavigationDelegate 
             })
     }
     
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated  {
+            if let url = navigationAction.request.url,
+                let host = url.host, !host.hasPrefix("www.google.com"),
+                UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+                print(url)
+                print("Redirected to browser. No need to open it locally")
+                decisionHandler(.cancel)
+            } else {
+                print("Open it locally")
+                decisionHandler(.allow)
+            }
+        } else {
+            print("not a user click")
+            decisionHandler(.allow)
+        }
+    }
+    
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         print("web view loading failed: \(error.localizedDescription)")
     }
